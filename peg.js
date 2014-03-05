47f1129599fca43b0c32c73e59fb105d86186f0c
c5f7c9 <c5f7c9@gmail.com>
2014-03-04 23:39:41 -0800

    PEG

---
+++ peg.js
@@ -0,0 +1,170 @@
+// PEG syntax of LL(k) grammars.
+// Exports LLKP.PEG class.
+// Depends on `core.then` module.
+
+!function () {
+    'use strict';
+
+    var core = typeof window != typeof void 0 ?
+        window.LLKP.Core : // for browsers
+        (require('./core.then'), require('./core')); // for Node
+
+    var Pattern = core.Pattern;
+    var txt = core.txt;
+    var rgx = core.rgx;
+    var opt = core.opt;
+    var exc = core.exc;
+    var any = core.any;
+    var seq = core.seq;
+    var rep = core.rep;
+
+    function PEG(definition, rules) {
+        var refs = {};
+
+        function parse(rule) {
+            var r = PEG.pattern.exec(rule);
+            if (r) return r;
+            throw new SyntaxError('Invalid PEG rule: ' + rule);
+        }
+
+        function compile(ast) {
+            if ('ref' in ast) return ref(ast.ref);
+            if ('txt' in ast) return txt(ast.txt);
+            if ('rgx' in ast) return rgx(new RegExp(ast.rgx));
+            if ('opt' in ast) return opt(compile(ast.opt));
+            if ('not' in ast) return not(compile(ast.not));
+            if ('def' in ast) return compile_def(ast);
+            if ('seq' in ast) return compile_seq(ast.seq);
+            if ('any' in ast) return compile_alt(ast.any);
+            if ('sep' in ast) return compile_rep(ast);
+            if ('exc' in ast) return exc(compile(ast.exc.lhs), compile(ast.exc.rhs));
+        }
+
+        function compile_rep(ast) {
+            return rep(compile(ast.rep), ast.sep && compile(ast.sep), ast.min);
+        }
+
+        function compile_def(ast) {
+            var p = compile(ast.def);
+            return !ast.key ? p : p.select(ast.key);
+        }
+
+        function compile_alt(asts) {
+            var p = asts.map(compile);
+            return p.length > 1 ? any.apply(0, p) : p[0];
+        }
+
+        function compile_seq(asts) {
+            var m, p = asts.map(compile);
+
+            asts.forEach(function (ast, i) {
+                if ('lbl' in ast) {
+                    m = m || {};
+                    m[ast.lbl] = i;
+                }
+            });
+
+            return m ? seq.apply(0, p).map(m) : p.length > 1 ? seq.apply(0, p) : p[0];
+        }
+
+        function build(definition, name) {
+            if (definition instanceof RegExp)
+                return rgx(definition);
+
+            if (definition instanceof Function)
+                return new Pattern(name || definition, definition);
+
+            if (definition instanceof Pattern)
+                return definition;
+
+            return compile(parse(definition + ''));
+        }
+
+        function ref(name) {
+            return (refs[name] = refs[name]) || new Pattern(name, function (str, pos) {
+                return refs[name].exec(str, pos);
+            });
+        }
+
+        function init(self) {
+            var pattern, name;
+
+            if (rules instanceof Function)
+                rules.call(rules = {}, build);
+
+            for (name in rules)
+                refs[name] = build(rules[name], name);
+
+            pattern = build(definition);
+
+            for (name in refs)
+                if (!refs[name])
+                    throw new SyntaxError('Rule is not defined: ' + name);
+
+            Pattern.call(self, pattern + '', pattern.exec);
+        }
+
+        if (this instanceof PEG)
+            init(this);
+        else
+            return new PEG(definition, rules);
+    }
+
+    PEG.pattern = compose('def', function ($) {
+        this.alt = rep($('seq'), rgx(/\s+\/\s+/)).as('any');
+        this.seq = rep(any($('exc'), $('trm')), rgx(/\s+/)).as('seq');
+        this.exc = seq($('trm'), rgx(/\s+~\s+/), $('trm')).map({ lhs: 0, rhs: 2 }).as('exc');
+        this.atm = any($('txt'), $('grp'), $('chr'), $('ref'));
+        this.txt = any(str('"', '"'), str("'", "'")).as('txt');
+        this.chr = str('[', ']').text().as('rgx');
+        this.ref = rgx(/[a-z]+/i).as('ref');
+        this.trm = any(
+            seq($('lbl'), txt(':'), $('trm')).then(function (r) { r[2].lbl = r[0]; return r[2]; }),
+            seq(txt('&'), $('trm')).select(1).as('not').as('not'),
+            seq(txt('!'), $('trm')).select(1).as('not'),
+            seq($('atm'), txt('?')).select(0).as('opt'),
+            seq($('atm'), $('qtf')).then(function (r) { r[1].rep = r[0]; return r[1]; }),
+            $('atm'));
+
+        this.grp = seq(txt('('), $('def'), txt(')'), opt(seq(txt('.'), $('lbl')).select(1))).map({ def: 1, key: 3 });
+        this.qtf = seq(opt($('sep')), any(txt('+').make(1), txt('*').make(0))).map({ sep: 0, min: 1 });
+        this.sep = seq(txt('<'), $('def'), txt('>')).select(1);
+        this.lbl = rgx(/[a-z]+/i);
+        this.def = $('alt');
+    });
+
+    PEG.prototype = Pattern.prototype;
+
+    function compose(name, define) {
+        var rules = {};
+
+        define.call(rules, function (name) {
+            return rules[name] || new Pattern(name, function (str, pos) {
+                return rules[name].exec(str, pos);
+            });
+        });
+
+        return rules[name];
+    }
+
+    function str(lq, rq) {
+        var chr = any(
+            seq(txt('\\'), txt(lq)).select(1),
+            seq(txt('\\'), txt(rq)).select(1),
+            exc(rgx(/[\u0000-\uffff]/), txt(rq)));
+
+        return seq(txt(lq), opt(rep(chr), []).merge(), txt(rq)).select(1);
+    }
+
+    function not(pattern) {
+        return new Pattern('!' + pattern, function (str, pos) {
+            return !pattern.exec(str, pos) && { res: void 0, end: pos };
+        });
+    }
+
+    if (typeof module != typeof void 0) // for Node
+        module.exports = PEG;
+
+    if (typeof window != typeof void 0) // for browsers
+        window.LLKP.PEG = PEG;
+}(); // jshint ignore:line
---
+++ test/peg.js
@@ -0,0 +1,262 @@
+var assert = require('assert');
+var PEG = require('../peg');
+
+console.inspect = function (object) {
+    var inspect = require('util').inspect;
+    return console.log(inspect(object, { depth: null }));
+};
+
+function forEach(dict, fn) {
+    for (var key in dict)
+        fn(dict[key], key);
+}
+
+function ptest(pattern, samples) {
+    var rules = {
+        'num': PEG(/\d+/).parseInt(),
+        'var': /[a-zA-Z]\w+/
+    };
+
+    if (arguments.length == 3) {
+        rules = arguments[1];
+        samples = arguments[2];
+    }
+
+    forEach(samples, function (expectedResult, input) {
+        var testName = 'PEG(' + pattern + ').exec(' + input + ') = ' + expectedResult;
+        test(testName, function () {
+            var result = PEG(pattern, rules).exec(input);
+            assert.deepEqual(result, expectedResult);
+        });
+    });
+}
+
+suite('PEG', function () {
+    'use strict';
+
+    suite('BasicParsing', function () {
+        suite('Text', function () {
+            ptest('""', {
+                '': '',
+                'a': null
+            });
+
+            ptest('"\\"\x31\r\n\u0032\'"', {
+                '\"1\r\n2\'': '\"1\r\n2\'',
+                '123': null
+            });
+
+            ptest("''", {
+                '': '',
+                'a': null
+            });
+
+            ptest("'\"\x31\r\n\u0032\\''", {
+                '\"1\r\n2\'': '\"1\r\n2\'',
+                '123': null
+            });
+        });
+
+        suite('Class', function () {
+            ptest('[]', {
+                '': null,
+                'a': null
+            });
+
+            ptest('[a-f0-9]', {
+                'a': 'a',
+                'c': 'c',
+                'f': 'f',
+                '0': '0',
+                '5': '5',
+                '9': '9',
+                'A': null,
+                '': null
+            });
+
+            ptest('[^0-9]', {
+                'a': 'a',
+                '3': null,
+                '0': null,
+                '9': null
+            });
+        });
+
+        suite('PositiveLookAhead', function () {
+            ptest('&[5]', {
+                '5': null,
+                '': null
+            });
+
+            ptest('&[5] [0-9]', {
+                '5': [void 0, '5'],
+                '4': null,
+                '': null
+            });
+        });
+
+        suite('NegativeLookAhead', function () {
+            ptest('![5]', {
+                '5': null,
+                '': void 0
+            });
+
+            ptest('![5] [0-9]', {
+                '5': null,
+                '4': [void 0, '4'],
+                '': null
+            });
+        });
+
+        suite('Option', function () {
+            ptest('[0-9]?', {
+                '3': '3',
+                '': void 0
+            });
+
+            ptest('[a-z] [0-9]?', {
+                'a4': ['a', '4'],
+                'a': ['a', void 0],
+                '5': null
+            });
+        });
+
+        suite('Group', function () {
+            ptest('("a" "b" "c")+', {
+                'abcabc': [['a', 'b', 'c'], ['a', 'b', 'c']],
+                'abca': null,
+                '': null
+            });
+        });
+
+        suite('Alternation', function () {
+            ptest('[a-z] / [0-9]', {
+                'd': 'd',
+                '4': '4',
+                'F': null,
+                '': null
+            });
+
+            ptest('[a] [b] / [c] [d]', {
+                'ab': ['a', 'b'],
+                'cd': ['c', 'd'],
+                'ac': null,
+                'abcd': null,
+                '': null
+            });
+        });
+
+        suite('Sequence', function () {
+            ptest('[a] [b] [c]', {
+                'abc': ['a', 'b', 'c'],
+                '': null
+            });
+
+            ptest('[a] [a-z] ~ [h] [b]', {
+                'akb': ['a', 'k', 'b'],
+                'ahb': null,
+                'qwe': null,
+                '': null
+            });
+
+            ptest('word:[a-z]+', {
+                'abc': { word: ['a', 'b', 'c'] },
+                'd': { word: ['d'] },
+                '': null
+            });
+
+            ptest('a:[0-9] / b:[a-z]', {
+                '4': { a: '4' },
+                't': { b: 't' },
+                '': null
+            });
+
+            ptest('a:[0-9] b:[a-z]', {
+                '7g': { a: '7', b: 'g' },
+                'rr': null
+            });
+        });
+
+        suite('Exclusion', function () {
+            ptest('[\x00-\xFF] ~ [0-9]', {
+                'q': 'q',
+                'G': 'G',
+                '4': null,
+                '': null
+            });
+
+            ptest('[a] ~ [a]', {
+                'a': null,
+                '': null
+            });
+        });
+
+        suite('Repetition', function () {
+            ptest('[0-9]+', {
+                '123': ['1', '2', '3'],
+                '2': ['2'],
+                'q': null,
+                '': null
+            });
+
+            ptest('[0-9]*', {
+                '123': ['1', '2', '3'],
+                '2': ['2'],
+                'q': null,
+                '': []
+            });
+
+            ptest('[0-9]<[;,]>+', {
+                '1,2;3,4': ['1', '2', '3', '4'],
+                '1-2+3': null,
+                '4': ['4'],
+                '': null
+            });
+
+            ptest('[0-9]<[;,]>*', {
+                '1,2;3,4': ['1', '2', '3', '4'],
+                '1-2+3': null,
+                '4': ['4'],
+                '': []
+            });
+        });
+    });
+
+    suite('PracticalApplications', function () {
+        ptest('[+-]? [0-9]+ ("." [0-9]+)? ([eE] [+-]? [0-9]+)?', {
+            "5": [void 0, ["5"], void 0, void 0],
+            "-5": ["-", ["5"], void 0, void 0],
+            "1.23": [void 0, ["1"], [".", ["2", "3"]], void 0],
+            "4e-9": [void 0, ["4"], void 0, ["e", "-", ["9"]]],
+            "4e19": [void 0, ["4"], void 0, ["e", void 0, ["1", "9"]]],
+            '+12.34567e-890': ["+", ["1", "2"], [".", ["3", "4", "5", "6", "7"]], ["e", "-", ["8", "9", "0"]]]
+        });
+
+        test('JSON', function () {
+            // The ABNF grammar of JSON was taken from RFC 4627 and translated into PEG.
+            var pattern = new PEG('object / array', function (rule) {
+                this['object'] = rule('"{" (string ":" value)<",">* "}"').select(1).join(0, 2);
+                this['array'] = rule('"[" value<",">* "]"').select(1);
+                this['value'] = 'object / array / number / string / false / true / null';
+                this['false'] = rule('"false"').make(false);
+                this['true'] = rule('"true"').make(true);
+                this['null'] = rule('"null"').make(null);
+                this['number'] = rule('[-]? [0-9]+ ("." [0-9]+)? ("e" [+-] [0-9]+)?').text().parseFloat();
+                this['string'] = rule('["] char* ["]').select(1).merge();
+                this['char'] = '[\x00-\xFF] ~ ["]'; // RFC 4627 specifies a more complicated rule for this
+            });
+
+            var source = {
+                "num": -1.2345e-11,
+                "str": "abc def",
+                "true": true,
+                "false": false,
+                "null": null,
+                "object": { "a": +1, "b": -2, "c": 0 },
+                "array": [{}, { "x": "y" }, "21", 23.22, [], "", null, true, false]
+            };
+
+            assert.deepEqual(pattern.exec(JSON.stringify(source)), source);
+        });
+    });
+});
